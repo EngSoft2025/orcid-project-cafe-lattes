cebe6558c56bf0e5b617c76b6a2404fb95d178be
clr <clrcera05@gmail.com>
2025-05-28 09:52:57 -0300

    Initial Orcid Work

--- backend/model/work.go
+++ backend/model/work.go
@@ -1,6 +1,35 @@
 package model
 
 type (
-	OrcidWork struct{}
-	WorkData  struct{}
+	WorkData struct{}
 )
+
+type OrcidWork struct {
+	Group []struct {
+		WorkSummary []struct {
+			PutCode string `json:"put-code"`
+			Title   struct {
+				Title struct {
+					Value string `json:"value"`
+				} `json:"title"`
+			} `json:"title"`
+			ExternalIds struct {
+				ExternalId []struct {
+					Value string `json:"external-id-value"`
+				} `json:"external-id"`
+			} `json:"external-ids"`
+			Url struct {
+				Value string `json:"value"`
+			} `json:"url"`
+			Type            string `json:"type"`
+			PublicationDate struct {
+				Year struct {
+					Value string `json:"value"`
+				} `json:"year"`
+			} `json:"publication-date"`
+			JournalTitle struct {
+				Value string `json:"value"`
+			} `json:"journal-title"`
+		} `json:"work-summary"`
+	} `json:"group"`
+}
